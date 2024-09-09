e8eaec2cf2c43b5b5a501cf5599e39f4eddf1620
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2024-09-09 10:10:21 +0200

    change gaps between infolines with css modification to sunrise and sunset

--- frontend/src/components/timeresult/sunset/Sunset.jsx
+++ frontend/src/components/timeresult/sunset/Sunset.jsx
@@ -1,14 +1,17 @@
 import styles from "./Sunset.module.css";
 
-function Sunset({result}){
+function Sunset({result}) {
     return (
         <div className={styles.sunset}>
-            <p>Time of sunset</p>
-            <h1>{result.time}</h1>
-            <p>{result.date}</p>
-            <p>{result.city.name}, {result.city.country}, {result.city.state}</p>
-            <p>Longitude: {result.city.lon}</p>
-            <p>Latitude: {result.city.lat}</p>
+            <p className={styles.timeOfSunsetP}>Time of sunset</p>
+            <h1 className={styles.time}>{result.time}</h1>
+            <h4 className={styles.utc}>(UTC)</h4>
+            <div className={styles.infoDiv}>
+                <p>{result.date}</p>
+                <p>{result.city.name}, {result.city.country}, {result.city.state}</p>
+                <p>Longitude: {result.city.lon}</p>
+                <p>Latitude: {result.city.lat}</p>
+            </div>
         </div>
     )
 }
