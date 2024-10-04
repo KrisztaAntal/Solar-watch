3aca62493226309ef9affafa43b98e6cc5d2a237
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2024-10-04 22:05:23 +0200

    add error message on wrong username and/or password on login

--- frontend/src/components/loginform/LoginForm.jsx
+++ frontend/src/components/loginform/LoginForm.jsx
@@ -1,45 +1,57 @@
-import styles from "./LoginForm.module.css"
-import {useState} from "react";
+import styles from "./LoginForm.module.css";
+import {useEffect, useState} from "react";
 import {useUser} from "../../context/UserProvider.jsx";
 import {useNavigate} from "react-router-dom";
 
 function LoginForm() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginError, setLoginError] = useState(false);
+    const [firstAttempt, setFirstAttempt] = useState(true);
     const navigate = useNavigate();
-    const {login} = useUser();
+    const {login, loggedIn} = useUser();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         login({
             username,
             password
-        })
-        navigate("/solar-watch")
+        });
+        setFirstAttempt(false)
     }
 
+    useEffect(() => {
+        if (loggedIn) {
+            navigate("/solar-watch");
+            setLoginError(false);
+        } else {
+            if (!firstAttempt) {
+                setLoginError(true);
+            }
+        }
+    }, [loggedIn, navigate, firstAttempt])
+
     return (
-        <>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <div className={styles.formInput}>
-                    <label>Username:<br/>
-                        <input value={username}
-                               onChange={(e) => setUsername(e.target.value)}
-                               name={"username"}
-                               id={"username"}/>
-                    </label>
-                </div>
-                <div className={styles.formInput}>
-                    <label>Password:<br/>
-                        <input type={"password"} value={password}
-                               onChange={(e) => setPassword(e.target.value)}
-                               name={"password"}
-                               id={"password"}/><br/>
-                    </label>
-                </div>
-                <button className={styles.formButton}><h2>Login</h2></button>
-            </form>
-        </>
+        <form className={styles.form} onSubmit={handleSubmit}>
+            {loginError && <p className={styles.error}>Invalid username or password.</p>}
+            <div className={styles.formInput}>
+                <label>Username:<br/>
+                    <input value={username}
+                           onChange={(e) => setUsername(e.target.value)}
+                           name={"username"}
+                           id={"username"}/>
+                </label>
+            </div>
+            <div className={styles.formInput}>
+                <label>Password:<br/>
+                    <input type={"password"} value={password}
+                           onChange={(e) => setPassword(e.target.value)}
+                           name={"password"}
+                           id={"password"}/><br/>
+                </label>
+            </div>
+            <button className={styles.formButton}><h2>Login</h2></button>
+        </form>
     )
 }
 
