ad791c2460f9edde8ef5b66a2a16bd30c564d730
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2024-10-04 22:02:41 +0200

    set menu to close on logout too in mobile view

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import {Link, Outlet, useNavigate} from 'react-router-dom';
+import React, {useEffect, useState} from "react";
+import {Link, Outlet, useNavigate} from "react-router-dom";
 import styles from "./NavBar.module.css";
-import {useEffect, useState} from "react";
 import Footer from "../footer/Footer.jsx";
 import {useUser} from "../../context/UserProvider.jsx";
 import logo from "../../assets/solar-watch-logo.svg";
@@ -14,11 +13,13 @@ function NavBar() {
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
-    };
+    }
 
     useEffect(() => {
         if (user) {
             setIsLoggedIn(true)
+        } else {
+            setIsLoggedIn(false)
         }
     }, [user])
 
@@ -29,7 +30,7 @@ function NavBar() {
                 <button className={styles.hamburger} onClick={toggleMenu}>
                     ☰
                 </button>
-                <div className={`${styles.navLinks} ${isOpen ? styles.navLinksOpen : ''}`}>
+                <div className={`${styles.navLinks} ${isOpen ? styles.navLinksOpen : ""}`}>
                     {!isLoggedIn &&
                         <>
                             <Link to={"/login"} onClick={() => setIsOpen(false)}>
@@ -40,18 +41,21 @@ function NavBar() {
                             </Link>
                         </>
                     }
-                    {isLoggedIn && <>
-                        <Link to={"/solar-watch"} onClick={() => setIsOpen(false)}>
-                            Get times
-                        </Link>
-                        <Link to={"/user-page"} onClick={() => setIsOpen(false)}>
-                            User page
-                        </Link>
-                        <Link to={"/"} onClick={() => {
-                            logout();
-                            setIsLoggedIn(false);
-                        }}>Logout</Link>
-                    </>
+                    {isLoggedIn &&
+                        <>
+                            <Link to={"/solar-watch"} onClick={() => setIsOpen(false)}>
+                                Get times
+                            </Link>
+                            <Link to={"/user-page"} onClick={() => setIsOpen(false)}>
+                                User page
+                            </Link>
+                            <Link to={"/"} onClick={() => {
+                                logout();
+                                setIsOpen(false);
+                            }}>
+                                Logout
+                            </Link>
+                        </>
                     }
                 </div>
             </nav>
